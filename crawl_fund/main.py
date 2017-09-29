208f98d0a759e624692e7a6194f3c95c29fbaca6
seven <seven_2016@163.com>
2017-09-29 10:12:15 +0800

    新增了文件读取解析以及入库

--- crawl_fund/main.py
+++ crawl_fund/main.py
@@ -8,6 +8,14 @@
 '''
 from selenium import webdriver
 from selenium.webdriver.support.ui import  WebDriverWait
+from bs4 import BeautifulSoup
+import os
+import time
+from sqlalchemy import Column,String,Integer,create_engine
+from sqlalchemy.orm import sessionmaker
+from crawl_fund.jijin import JijinDatum
+
+
 
 ##获取总的页数
 def getPageTotal(driver):
@@ -31,14 +39,50 @@ def getData(driver,start,end):
             f.write(driver.find_element_by_id("tableDiv").get_attribute("innerHTML").encode('utf-8'))
             f.close()
 def Soup():
-    pass
+    #读取htmls文件夹的每一个文件
+    for filename in os.listdir(os.getcwd()+'/htmls'):
+        with open(os.getcwd()+'/htmls/'+filename,'r',encoding='UTF-8') as file:
+                print(filename)
+                soup=BeautifulSoup(file.read(),'html.parser')
+                ##获取基金代码
+                code=[code.get_text() for code in soup.find_all('td',class_='bzdm')]
+                name=[name.contents[0].contents[0].get_text() for name in soup.find_all("td",class_='tol')]
+                ##基金增长值和增长是会改变的,变为black,green，所以合并这三种类型，就涵盖了全部
+                group_value_red=[group_value.get_text() for group_value in  soup.find_all("td",class_='rzzz red')]
+                group_value_black =[group_value.get_text() for group_value in soup.find_all("td", class_='rzzz black')]
+                group_value_green = [group_value.get_text() for group_value in soup.find_all("td", class_='rzzz green')]
+                group_value=group_value_red+group_value_black+group_value_green
+
+                group_rate_red1=[group_rate.get_text() for group_rate in soup.find_all("td", class_='rzzl bg red')]
+                group_rate_red2 = [group_rate.get_text() for group_rate in soup.find_all("td", class_='bg rzzl red')]
+                group_rate_black=[group_rate.get_text() for group_rate in soup.find_all("td", class_='bg rzzl black')]
+                group_rate_green = [group_rate.get_text() for group_rate in soup.find_all("td", class_='bg rzzl green')]
+                group_rate=group_rate_red1+group_rate_red2+group_rate_black+group_rate_green
+
+                for count in range(0,len(code)):
+                    jijin_code = code[count]
+                    jijin_name = name[count]
+                    jijin_group_value = group_value[count]
+                    jijin_group_rate = group_rate[count]
+                    data = {'code': jijin_code, 'name': jijin_name, 'grow_value': jijin_group_value,
+                            'grow_rate': jijin_group_rate}
+                    inserDb(data)
+def inserDb(data):
+    engine = create_engine('mysql+pymysql://root:hello2016@localhost/jijin?charset=utf8')
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    newfuns=JijinDatum(code=data['code'],name=data['name'],grow_value=data['grow_value'],grow_rate=data['grow_rate'],insert_date=time.strftime('%Y-%m-%d',time.localtime(time.time())))
+    session.add(newfuns)
+    session.commit()
+    session.close()
 
 if __name__=='__main__':
-    # 初始化selenium
+    #初始化selenium
     driver = webdriver.PhantomJS()
     driver.get('http://fund.eastmoney.com/fund.html')
     totalpage=getPageTotal(driver)
     getData(driver,1,totalpage)
+    Soup()
 
 
 
