cf9623cb68f5bd6cd2ea2db52785752dc90acf00
seven <seven_2016@163.com>
2017-10-07 22:06:39 +0800

    stash

--- crawl_fund/Spiders/Fund.py
+++ crawl_fund/Spiders/Fund.py
@@ -80,10 +80,9 @@ def SaveDb():
                 ##构造数据库实体化对象列表，方便批量插入
                 dataList.append(myfund)
     #批量插入
-    print(dataList)
-    # mysession.add_all(dataList)  # 批量新增
-    # mysession.commit()
-    # mysession.close()
+    mysession.add_all(dataList)  # 批量新增
+    mysession.commit()
+    mysession.close()
 ##获取基金每一页的数据，并且写入到文件当中
 def getFundhtml():
     # 初始化selenium
