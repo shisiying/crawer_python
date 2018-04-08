2241c4ce5dac466bcaaf2614897ca00904d5e304
shisiying <751401459@qq.com>
2018-04-08 14:22:11 +0800

    Update config.py

--- dingding/common/config.py
+++ dingding/common/config.py
@@ -1,4 +1,4 @@
 # sqlacodegen --tables returnvisit --outfile Returnvisit.py mysql+pymysql://root:mysql123456@1.85.18.26/jtdz?charset=utf8
-dburl="mysql+pymysql://root:mysql123456@1.85.18.26/jtdz?charset=utf8"
-corpid = 'ding95d0e17f21c9b99a'
-corpsecret = 'sn8LZ2Vg-ryUtk9YcyyGoIcRBfJ7NoevxwUlh4eXXaykwBpKka3P2QMjitc1fElk'
+dburl="mysql+pymysql://root:mysql123456@1.185.118.26/jtdz?charset=utf8"
+corpid = 'ding95d0e17f21c9bFDFD99a' ##假的id
+corpsecret = 'sn8LZ2Vg-ryUtk9YcyyGoIcRBfJ7NoevxwUlh4eXXaySDkwBpKkDFDSDSa3P2QMjitc1fElk' ##假的
